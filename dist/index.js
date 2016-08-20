b27743f0dd607ee3525d279bdee93ebdc1a4c538
Jonathan Glanz <jonathan.glanz@gmail.com>
2016-08-20 17:34:05 -0400

    Created epictask-workspace

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
+
 function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+    for (var p in m) {
+        if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+    }
 }
 __export(require('./Typedux'));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBLGlCQUFjLFdBQVcsQ0FBQyxFQUFEIn0=
+//# sourceMappingURL=index.js.map
