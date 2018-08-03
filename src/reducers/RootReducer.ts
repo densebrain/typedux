9ba73e72c59caeb1b7a9714414ce16925b8bde73
Jonathan Glanz <jonathan.glanz@gmail.com>
2018-08-03 16:33:53 -0700

    - merged

--- src/reducers/RootReducer.ts
+++ src/reducers/RootReducer.ts
@@ -11,11 +11,11 @@ import * as isEqualShallow from 'shallowequal'
 import {INTERNAL_ACTIONS, INTERNAL_ACTION} from "../Constants"
 
 const
-  _ = require('lodash')
+	_ = require('lodash')
 
 const
-  ActionIdCacheMax = 500,
-  log = getLogger(__filename)
+	ActionIdCacheMax = 500,
+	log = getLogger(__filename)
 
 
 /**
@@ -26,10 +26,10 @@ const
  * @returns {any|T|undefined|V|string|IDBRequest}
  */
 function getLeafValue(rootValue, leaf:string) {
-  return rootValue &&
-         (rootValue.get ?
-           rootValue.get(leaf) :
-           rootValue[leaf])
+	return rootValue &&
+		(rootValue.get ?
+			rootValue.get(leaf) :
+			rootValue[leaf])
 }
 
 /**
@@ -44,244 +44,244 @@ export type RootReducerErrorHandler = (err:Error, reducer?:ILeafReducer<any, any
  * to the appropriate state functions
  */
 export class RootReducer<S extends State<any>> {
-  
-  // Internal list of all leaf reducers
-  private reducers:ILeafReducer<any, ActionMessage<any>>[] = []
-  
-  // handled actions ids to avoid duplication
-  private handledActionIds = []
-  
-  /**
-   * onError ref, allows an error handler to
-   * be assigned to the reducer
-   */
-  public onError:RootReducerErrorHandler
-  
-  
-  /**
-   * Create reducer
-   *
-   * @param rootStateType - type of root state, must be immutable map or record
-   * @param reducers - list of all child reducers
-   */
-  constructor(private rootStateType:{ new():S } = null, ...reducers:ILeafReducer<any, any>[]) {
-    const leafs = []
-    reducers
-      .filter(reducer => isFunction(reducer.leaf))
-      .forEach(reducer => {
-        const leaf = reducer.leaf()
-        if (leafs.includes(leaf)) {
-          return
-        }
-        
-        leafs.push(leaf)
-        this.reducers.push(reducer)
-      })
-    
-    reducers
-      .filter(reducer => !isFunction(reducer.leaf))
-      .forEach(reducer => this.reducers.push(reducer))
-  }
-  
-  
-  /**
-   * Create default state
-   *
-   * @param defaultStateValue - if provided then its used as base for inflation
-   * @returns {State}
-   */
-  defaultState(defaultStateValue:any = null):S {
-    
-    
-    // LOAD THE STATE AND VERIFY IT IS Immutable.Map/Record
-    let
-      state = defaultStateValue || {type: "ROOT"} as State<any>
-    
-    
-    // ITERATE REDUCERS & CREATE LEAF STATES
-    this.reducers
-      .filter(reducer => isFunction(reducer.leaf))
-      .forEach(reducer => {
-        const
-          leaf = reducer.leaf(),
-          leafDefaultState = getLeafValue(defaultStateValue, leaf)
-        
-        state = {...state, [leaf]: reducer.defaultState(leafDefaultState || {})}
-      })
-    
-    
-    return state as any
-  }
-  
-  /**
-   * Create a generic handler for dispatches
-   *
-   * @returns {(state:S, action:ReduxAction)=>S}
-   */
-  makeGenericHandler():ReduxReducer<S> {
-    return (state:S, action:ReduxAction):S => this.handle(state, action as ActionMessage<any>) as S
-  }
-  
-  /**
-   * Handle action message
-   *
-   * @param state
-   * @param action
-   * @returns {State}
-   */
-  handle(state:S, action:ActionMessage<any>):S {
-    
-    // Check if action has already been processed
-    if (action.id && this.handledActionIds.includes(action.id)) {
-      if (typeof console !== 'undefined' && console.trace) {
-        console.trace(`Duplicate action received: ${action.leaf}/${action.type}, ${action.id}`, action)
-      }
-      return state as S
-    }
-    
-    // Push action id to the handled list
-    if (action.id) {
-      this.handledActionIds.unshift(action.id)
-      if (this.handledActionIds.length > ActionIdCacheMax) {
-        this.handledActionIds.length = ActionIdCacheMax
-      }
-    }
-    try {
-      let hasChanged = false
-      
-      // Guard state type as immutable
-      if (!state) {
-        state = this.defaultState(state)
-        hasChanged = true
-      }
-      
-      const stateMap:State<any> = state as any
-      
-      // Iterate leafs and execute actions
-      let tempState = {...stateMap}
-      for (let reducer of this.reducers) {
-        if (isFunction(reducer)) {
-          const simpleReducer = reducer as any
-          const simpleState = simpleReducer(tempState,action)
-          if (simpleState !== tempState) {
-            tempState = simpleState
-            hasChanged = true
-          }
-          continue
-        }
-        
-        const
-          // Get the reducer leaf
-          leaf = reducer.leaf(),
-          
-          // Get Current RAW state
-          rawLeafState = tempState[leaf],
-          
-          // Shape it for the reducer
-          startReducerState = rawLeafState
-        
-        let
-          reducerState = startReducerState,
-          stateChangeDetected = false
-        
-        try {
-          
-          /**
-           * Check the returned state from every handler for changes
-           *
-           * @param newReducerState
-           */
-          const checkReducerStateChange = (newReducerState) => {
-            if (!newReducerState) {
-              throw new Error(`New reducer state is null for leaf ${leaf}`)
-            }
-            
-            
-            stateChangeDetected = stateChangeDetected || !isEqualShallow(reducerState, newReducerState)
-            reducerState = {...newReducerState}
-            //log.debug("State change detected",stateChangeDetected)
-          }
-          
-          // Check internal actions
-          if (INTERNAL_ACTIONS.includes(action.type)) {
-            log.debug(`Sending init event to ${leaf} - internal action received ${action.type}`)
-            
-            if (INTERNAL_ACTION.INIT === action.type && reducer.init) {
-              checkReducerStateChange(reducer.init(startReducerState))
-            }
-          }
-          
-          // Check leaf of reducer and action to see if this reducer handles the supplied action
-          if (action.leaf && action.leaf !== leaf) {
-            continue
-          }
-          
-          // Get the action registration
-          const actionReg = getAction(leaf, action.type)
-          
-          log.debug('Action type supported', leaf, action.type)
-          
-          // CHECK REDUCER.HANDLE
-          if (reducer.handle) {
-            checkReducerStateChange(reducer.handle(reducerState, action))
-          }
-          
-          // ActionMessage.reducers PROVIDED
-          if (action.stateType && reducerState instanceof action.stateType) {
-            _.get(action, 'reducers', []).forEach((actionReducer) =>
-              checkReducerStateChange(actionReducer(reducerState, action)))
-          }
-          
-          
-          // IF @ActionReducer REGISTERED
-          if (actionReg && actionReg.options.isReducer) {
-            const reducerFn = actionReg.action(null, ...action.args)
-            if (!reducerFn || !isFunction(reducerFn)) {
-              //noinspection ExceptionCaughtLocallyJS
-              throw new Error(`Action reducer did not return a function: ${actionReg.type}`)
-            }
-            
-            log.debug(`Calling action reducer: ${actionReg.fullName}`)
-            checkReducerStateChange(reducerFn(reducerState, getStoreStateProvider()))
-          }
-          
-          // CHECK ACTUAL REDUCER FOR SUPPORT
-          if (isFunction(reducer[action.type])) {
-            checkReducerStateChange(reducer[action.type](reducerState, ...action.args))
-          }
-          
-        } catch (err) {
-          log.error(`Error occurred on reducer leaf ${leaf}`, err)
-          if (reducer.handleError) {
-            reducer.handleError(startReducerState, action, err)
-          }
-          
-          this.onError && this.onError(err, reducer)
-        }
-        
-        if (stateChangeDetected) {
-          tempState = {...tempState, [leaf]: reducerState}
-          hasChanged = true
-        }
-      }
-      
-      
-      log.debug('Has changed after all reducers', hasChanged, 'states equal', isEqualShallow(tempState, state))
-      return (hasChanged ? tempState : state) as S
-      
-    } catch (err) {
-      log.error('Error bubbled to root reducer', err)
-      
-      // If error handler exists then use it
-      if (this.onError) {
-        this.onError && this.onError(err)
-        return state as S
-      }
-      
-      // Otherwise throw
-      throw err
-    }
-  }
+	
+	// Internal list of all leaf reducers
+	private reducers:ILeafReducer<any, ActionMessage<any>>[] = []
+	
+	// handled actions ids to avoid duplication
+	private handledActionIds = []
+	
+	/**
+	 * onError ref, allows an error handler to
+	 * be assigned to the reducer
+	 */
+	public onError:RootReducerErrorHandler
+	
+	
+	/**
+	 * Create reducer
+	 *
+	 * @param rootStateType - type of root state, must be immutable map or record
+	 * @param reducers - list of all child reducers
+	 */
+	constructor(private rootStateType:{ new():S } = null, ...reducers:ILeafReducer<any, any>[]) {
+		const leafs = []
+		reducers
+			.filter(reducer => isFunction(reducer.leaf))
+			.forEach(reducer => {
+				const leaf = reducer.leaf()
+				if (leafs.includes(leaf)) {
+					return
+				}
+				
+				leafs.push(leaf)
+				this.reducers.push(reducer)
+			})
+		
+		reducers
+			.filter(reducer => !isFunction(reducer.leaf))
+			.forEach(reducer => this.reducers.push(reducer))
+	}
+	
+	
+	/**
+	 * Create default state
+	 *
+	 * @param defaultStateValue - if provided then its used as base for inflation
+	 * @returns {State}
+	 */
+	defaultState(defaultStateValue:any = null):S {
+		
+		
+		// LOAD THE STATE AND VERIFY IT IS Immutable.Map/Record
+		let
+			state = defaultStateValue || {type: "ROOT"} as State<any>
+		
+		
+		// ITERATE REDUCERS & CREATE LEAF STATES
+		this.reducers
+			.filter(reducer => isFunction(reducer.leaf))
+			.forEach(reducer => {
+				const
+					leaf = reducer.leaf(),
+					leafDefaultState = getLeafValue(defaultStateValue, leaf)
+				
+				state = {...state, [leaf]: reducer.defaultState(leafDefaultState || {})}
+			})
+		
+		
+		return state as any
+	}
+	
+	/**
+	 * Create a generic handler for dispatches
+	 *
+	 * @returns {(state:S, action:ReduxAction)=>S}
+	 */
+	makeGenericHandler():ReduxReducer<S> {
+		return (state:S, action:ReduxAction):S => this.handle(state, action as ActionMessage<any>) as S
+	}
+	
+	/**
+	 * Handle action message
+	 *
+	 * @param state
+	 * @param action
+	 * @returns {State}
+	 */
+	handle(state:S, action:ActionMessage<any>):S {
+		
+		// Check if action has already been processed
+		if (action.id && this.handledActionIds.includes(action.id)) {
+			if (typeof console !== 'undefined' && console.trace) {
+				console.trace(`Duplicate action received: ${action.leaf}/${action.type}, ${action.id}`, action)
+			}
+			return state as S
+		}
+		
+		// Push action id to the handled list
+		if (action.id) {
+			this.handledActionIds.unshift(action.id)
+			if (this.handledActionIds.length > ActionIdCacheMax) {
+				this.handledActionIds.length = ActionIdCacheMax
+			}
+		}
+		try {
+			let hasChanged = false
+			
+			// Guard state type as immutable
+			if (!state) {
+				state = this.defaultState(state)
+				hasChanged = true
+			}
+			
+			const stateMap:State<any> = state as any
+			
+			// Iterate leafs and execute actions
+			let tempState = {...stateMap}
+			for (let reducer of this.reducers) {
+				if (isFunction(reducer)) {
+					const simpleReducer = reducer as any
+					const simpleState = simpleReducer(tempState, action)
+					if (simpleState !== tempState) {
+						tempState = simpleState
+						hasChanged = true
+					}
+					continue
+				}
+				
+				const
+					// Get the reducer leaf
+					leaf = reducer.leaf(),
+					
+					// Get Current RAW state
+					rawLeafState = tempState[leaf],
+					
+					// Shape it for the reducer
+					startReducerState = rawLeafState
+				
+				let
+					reducerState = startReducerState,
+					stateChangeDetected = false
+				
+				try {
+					
+					/**
+					 * Check the returned state from every handler for changes
+					 *
+					 * @param newReducerState
+					 */
+					const checkReducerStateChange = (newReducerState) => {
+						if (!newReducerState) {
+							throw new Error(`New reducer state is null for leaf ${leaf}`)
+						}
+						
+						
+						stateChangeDetected = stateChangeDetected || !isEqualShallow(reducerState, newReducerState)
+						reducerState = {...newReducerState}
+						//log.debug("State change detected",stateChangeDetected)
+					}
+					
+					// Check internal actions
+					if (INTERNAL_ACTIONS.includes(action.type)) {
+						log.debug(`Sending init event to ${leaf} - internal action received ${action.type}`)
+						
+						if (INTERNAL_ACTION.INIT === action.type && reducer.init) {
+							checkReducerStateChange(reducer.init(startReducerState))
+						}
+					}
+					
+					// Check leaf of reducer and action to see if this reducer handles the supplied action
+					if (action.leaf && action.leaf !== leaf) {
+						continue
+					}
+					
+					// Get the action registration
+					const actionReg = getAction(leaf, action.type)
+					
+					log.debug('Action type supported', leaf, action.type)
+					
+					// CHECK REDUCER.HANDLE
+					if (reducer.handle) {
+						checkReducerStateChange(reducer.handle(reducerState, action))
+					}
+					
+					// ActionMessage.reducers PROVIDED
+					if (action.stateType && reducerState instanceof action.stateType) {
+						_.get(action, 'reducers', []).forEach((actionReducer) =>
+							checkReducerStateChange(actionReducer(reducerState, action)))
+					}
+					
+					
+					// IF @ActionReducer REGISTERED
+					if (actionReg && actionReg.options.isReducer) {
+						const reducerFn = actionReg.action(null, ...action.args)
+						if (!reducerFn || !isFunction(reducerFn)) {
+							//noinspection ExceptionCaughtLocallyJS
+							throw new Error(`Action reducer did not return a function: ${actionReg.type}`)
+						}
+						
+						log.debug(`Calling action reducer: ${actionReg.fullName}`)
+						checkReducerStateChange(reducerFn(reducerState, getStoreStateProvider()))
+					}
+					
+					// CHECK ACTUAL REDUCER FOR SUPPORT
+					if (isFunction(reducer[action.type])) {
+						checkReducerStateChange(reducer[action.type](reducerState, ...action.args))
+					}
+					
+				} catch (err) {
+					log.error(`Error occurred on reducer leaf ${leaf}`, err)
+					if (reducer.handleError) {
+						reducer.handleError(startReducerState, action, err)
+					}
+					
+					this.onError && this.onError(err, reducer)
+				}
+				
+				if (stateChangeDetected) {
+					tempState = {...tempState, [leaf]: reducerState}
+					hasChanged = true
+				}
+			}
+			
+			
+			log.debug('Has changed after all reducers', hasChanged, 'states equal', isEqualShallow(tempState, state))
+			return (hasChanged ? tempState : state) as S
+			
+		} catch (err) {
+			log.error('Error bubbled to root reducer', err)
+			
+			// If error handler exists then use it
+			if (this.onError) {
+				this.onError && this.onError(err)
+				return state as S
+			}
+			
+			// Otherwise throw
+			throw err
+		}
+	}
 }
 
 // Export the RootReducer class as the default
--- src/store/ObservableStore.ts
+++ src/store/ObservableStore.ts
@@ -1,7 +1,6 @@
 import {getLogger} from 'typelogger'
 
 
-
 import RootReducer from "../reducers/RootReducer"
 
 
@@ -17,13 +16,13 @@ import {
 
 import {getValue} from 'typeguard'
 import {isFunction, isString, nextTick} from '../util'
-import {State,ILeafReducer} from '../reducers'
-import StateObserver,{ TStateChangeHandler } from './StateObserver'
-import { DefaultLeafReducer } from "../reducers/DefaultLeafReducer"
-import { INTERNAL_KEY } from "../Constants"
-import { InternalState } from "../internal/InternalState"
-import { TRootState } from "../reducers/State"
-import { ActionMessage } from "../actions/ActionTypes"
+import {State, ILeafReducer} from '../reducers'
+import StateObserver, {TStateChangeHandler} from './StateObserver'
+import {DefaultLeafReducer} from "../reducers/DefaultLeafReducer"
+import {INTERNAL_KEY} from "../Constants"
+import {InternalState} from "../internal/InternalState"
+import {TRootState} from "../reducers/State"
+import {ActionMessage} from "../actions/ActionTypes"
 import DumbReducer from "../reducers/DumbReducer"
 
 const log = getLogger(__filename)
@@ -33,7 +32,7 @@ const log = getLogger(__filename)
  * Manage the redux store for RADS
  */
 export class ObservableStore<S extends TRootState> implements Store<S> {
-
+	
 	/**
 	 * Factory method for creating a new observable store
 	 *
@@ -44,19 +43,19 @@ export class ObservableStore<S extends TRootState> implements Store<S> {
 	 * @param defaultStateValue
 	 */
 	static createObservableStore<S extends TRootState>(
-		leafReducersOrStates:Array<ILeafReducer<any,any>|State<string>>,
+		leafReducersOrStates:Array<ILeafReducer<any, any> | State<string>>,
 		enhancer:StoreEnhancer<any> = null,
-		rootStateType:{new():S} = null,
+		rootStateType:{ new():S } = null,
 		defaultStateValue:any = null
 	):ObservableStore<S> {
 		let
-			leafReducers = leafReducersOrStates.filter(it => isFunction(getValue(() => (it as any).leaf))) as Array<ILeafReducer<any,any>>,
+			leafReducers = leafReducersOrStates.filter(it => isFunction(getValue(() => (it as any).leaf))) as Array<ILeafReducer<any, any>>,
 			leafStates = leafReducersOrStates.filter(it => !isFunction(getValue(() => (it as any).leaf)) && isString(getValue(() => (it as any).type))) as Array<State<string>>,
-			otherReducers = leafReducersOrStates.filter(it => isFunction(it)) as Array<ILeafReducer<any,any>>
+			otherReducers = leafReducersOrStates.filter(it => isFunction(it)) as Array<ILeafReducer<any, any>>
 		
-		leafReducers = [...otherReducers,...leafReducers, ...leafStates.map(state => new DumbReducer(state))]
+		leafReducers = [...otherReducers, ...leafReducers, ...leafStates.map(state => new DumbReducer(state))]
 		
-		return new ObservableStore(leafReducers,enhancer,rootStateType,defaultStateValue)
+		return new ObservableStore(leafReducers, enhancer, rootStateType, defaultStateValue)
 	}
 	
 	/**
@@ -65,9 +64,9 @@ export class ObservableStore<S extends TRootState> implements Store<S> {
 	 * @param {string | State<string>} statesOrKeys
 	 * @returns {Array<State<string>>}
 	 */
-	static makeSimpleReducers(...statesOrKeys:Array<string|State<string>>):Array<ILeafReducer<State<string>,any>> {
+	static makeSimpleReducers(...statesOrKeys:Array<string | State<string>>):Array<ILeafReducer<State<string>, any>> {
 		return statesOrKeys
-			.map(state => isString(state) ? {type:state} : state as State<string>)
+			.map(state => isString(state) ? {type: state} : state as State<string>)
 			.map(state => new DumbReducer(state))
 	}
 	
@@ -77,9 +76,8 @@ export class ObservableStore<S extends TRootState> implements Store<S> {
 	 * @returns {DefaultLeafReducer<InternalState, ActionMessage<InternalState>>}
 	 */
 	static makeInternalReducer() {
-		return DefaultLeafReducer.makeLeafReducer(INTERNAL_KEY,InternalState)
+		return DefaultLeafReducer.makeLeafReducer(INTERNAL_KEY, InternalState)
 	}
-	
 	
 	
 	public rootReducer:RootReducer<S>
@@ -87,39 +85,38 @@ export class ObservableStore<S extends TRootState> implements Store<S> {
 	private rootReducerFn
 	private store
 	private pendingTick
-
+	
 	constructor(
-		leafReducers:ILeafReducer<any,any>[],
+		leafReducers:ILeafReducer<any, any>[],
 		enhancer:StoreEnhancer<S> = null,
-		public rootStateType:{new():S} = null,
+		public rootStateType:{ new():S } = null,
 		public defaultStateValue:any = null) {
-
+		
 		this.createRootReducer(...leafReducers)
 		this.store = createStore(
 			this.rootReducerFn,
 			this.rootReducer.defaultState(defaultStateValue),
 			enhancer
 		)
-
+		
 		this.subscribe(() => {
 			log.debug('State changed - SCHEDULE NOTIFY')
 			this.scheduleNotification()
 		})
 	}
-
+	
 	/**
 	 * Create a new root reducer
 	 *
 	 * @param leafReducers
 	 * @returns {any}
 	 */
-	private createRootReducer(...leafReducers:ILeafReducer<any,any>[]) {
-		this.rootReducer = new RootReducer(this.rootStateType,...leafReducers)
+	private createRootReducer(...leafReducers:ILeafReducer<any, any>[]) {
+		this.rootReducer = new RootReducer(this.rootStateType, ...leafReducers)
 		this.rootReducerFn = this.rootReducer.makeGenericHandler()
-
+		
 		return this.rootReducerFn
 	}
-	
 	
 	
 	/**
@@ -130,13 +127,12 @@ export class ObservableStore<S extends TRootState> implements Store<S> {
 	getReduxStore() {
 		return this.store;
 	}
-
 	
-
+	
 	/**
 	 * Update the reducers
 	 */
-	replaceReducers(...leafReducers:ILeafReducer<any,any>[]):void {
+	replaceReducers(...leafReducers:ILeafReducer<any, any>[]):void {
 		const
 			rootReducerFn = this.createRootReducer(
 				ObservableStore.makeInternalReducer(),
@@ -145,15 +141,15 @@ export class ObservableStore<S extends TRootState> implements Store<S> {
 		
 		this.store.replaceReducer(rootReducerFn)
 	}
-
-	subscribe(listener:()=>void):Unsubscribe {
+	
+	subscribe(listener:() => void):Unsubscribe {
 		return this.getReduxStore().subscribe(listener);
 	}
-
+	
 	replaceReducer(nextReducer:Reducer<S>):void {
 		throw new Error("We don't play with no regular reducers ;)")
 	}
-
+	
 	/**
 	 * Retrieve the current state
 	 * @returns {*}
@@ -161,7 +157,7 @@ export class ObservableStore<S extends TRootState> implements Store<S> {
 	getState():S {
 		return this.getReduxStore().getState();
 	}
-
+	
 	getInternalState():InternalState {
 		return this.getState()[INTERNAL_KEY] as InternalState
 	}
@@ -175,30 +171,30 @@ export class ObservableStore<S extends TRootState> implements Store<S> {
 	dispatch<A extends ReduxAction>(action:A):A {
 		return this.getReduxStore().dispatch(action)
 	}
-
-
+	
+	
 	/**
 	 * Schedule notifications to go out on next tick
 	 */
 	scheduleNotification() {
 		if (this.pendingTick) return;
-
+		
 		this.pendingTick = nextTick(() => {
 			let state = this.getState();
-
+			
 			this.pendingTick = null;
 			this.observers.forEach((listener) => listener.onChange(state))
 		})
 	}
-
+	
 	/**
 	 *
 	 */
 	onChange() {
 		this.scheduleNotification();
 	}
-
-
+	
+	
 	/**
 	 * Observe state path for changes
 	 *
@@ -206,35 +202,35 @@ export class ObservableStore<S extends TRootState> implements Store<S> {
 	 * @param handler
 	 * @returns {function()} unsubscribe observer
 	 */
-	observe(path:string|string[], handler:TStateChangeHandler) {
-		let observer = new StateObserver(path,handler)
+	observe(path:string | string[], handler:TStateChangeHandler) {
+		let observer = new StateObserver(path, handler)
 		this.observers.push(observer)
-
+		
 		const removeObserver:any = () => {
 			if (observer.removed) {
 				log.debug("Already removed this observer", observer);
 				return;
 			}
-
-			this.observers.find((it,index) => {
+			
+			this.observers.find((it, index) => {
 				if (observer === it) {
-					this.observers.splice(index,1)
+					this.observers.splice(index, 1)
 					return true
 				}
-
+				
 				return false
 			})
-
+			
 			observer.removed = true
 		}
-
+		
 		removeObserver.observer = observer
 		return removeObserver
-
-
+		
+		
 	}
-
-
+	
+	
 }
 
 
