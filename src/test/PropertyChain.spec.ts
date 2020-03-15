9e652b2ea8f1676d0e4a0bbe1ed47768eed4a86d
Jonathan Glanz <jonathan.glanz@gmail.com>
2020-03-15 11:48:05 -0400

    resolving forked and corrupted prop chains

--- src/test/PropertyChain.spec.ts
+++ src/test/PropertyChain.spec.ts
@@ -22,119 +22,6 @@ import {Selector} from "../selectors/SelectorTypes"
 const
   log = getLogger(__filename)
 
-// installMockStoreProvider()
-//
-// function getDefaultState(reducer) {
-// 	return reducer.handle(null,{type:'@INIT'})
-// }
-//
-// const
-// 	MockKey = 'mock',
-// 	MockStateStr1 = 'my first string'
-//
-//
-// /**
-//  * Mock leaf state, dumb test state with test props
-//  */
-// class MockLeafState implements State<any> {
-// 	type = MockLeafState
-//
-// 	str1:string = MockStateStr1
-// 	str2:string
-//
-// 	constructor(props:any = {}) {
-//
-//
-// 		Object.assign(this,props)
-// 	}
-// }
-//
-// /**
-//  * Make root reducer
-//  *
-//  * @param {ILeafReducer<any, any>} leafReducers
-//  * @returns {RootReducer<State<any>>}
-//  */
-// function makeRootReducer(...leafReducersOrStates:Array<ILeafReducer<any,any>|State<string>>) {
-// 	let
-// 		leafReducers = leafReducersOrStates.filter(it => isFunction(getValue(() => (it as any).leaf))) as Array<ILeafReducer<any,any>>,
-// 		leafStates = leafReducersOrStates.filter(it => !isFunction(getValue(() => (it as any).leaf)) && isString(getValue(() => (it as any).type))) as Array<State<string>>
-//
-// 	leafReducers = [...leafReducers, ...leafStates.map(state => new DumbReducer(state))]
-//
-// 	return new RootReducer(null,...leafReducers)
-// }
-//
-// interface IMockState extends State<string> {
-// 	type: string
-// 	[key:string]:any
-// }
-//
-// /**
-//  * Typed action message
-//  */
-// interface MockMessage extends ActionMessage<MockLeafState> {
-//
-// }
-//
-// //
-// // class MockLeafReducer implements ILeafReducer<MockLeafState,MockMessage> {
-// //
-// // 	leaf():string {
-// // 		return MockKey;
-// // 	}
-// //
-// // 	prepareState(o:any) {
-// // 		return o
-// // 	}
-// //
-// //
-// // 	defaultState() {
-// // 		return new MockLeafState()
-// // 	}
-// // }
-//
-// // Simple mock factory
-// class MockActionFactory extends ActionFactory<MockLeafState,MockMessage> {
-//
-// 	constructor() {
-// 		super(MockLeafState)
-// 	}
-//
-// 	leaf():string {
-// 		return MockKey;
-// 	}
-//
-//
-//
-// 	@ActionReducer()
-// 	mockUpdate(val:string) {
-// 		return (state:IMockState) => ({...state, str1: val})
-// 	}
-//
-// 	@ActionReducer()
-// 	mockUpdateFromState(newVal:string) {
-// 		return (state:Map<string,any>) => ({...state, str2: newVal})
-// 	}
-//
-// 	@ActionThunk()
-// 	mockThunk() {
-// 		return Promised((dispatch,getState) => {
-// 			return Promise.delay(1000).then(() => "mock")
-// 		})
-// 	}
-//
-// 	@ActionThunk()
-// 	mockThunkError() {
-// 		return Promised((dispatch,getState) => {
-// 			return Promise.delay(1000).then(() => {
-// 				throw new Error('MockThunkErrorTest')
-// 			})
-// 		})
-// 	}
-// }
-
-
 describe('#selectors', function () {
   this.timeout(10000)
   
@@ -150,7 +37,7 @@ describe('#selectors', function () {
           }
         }
       },
-      selectorChainTests:Array<[Selector<any, any>, any, Array<string | number>]> = [
+      selectorChainTests:Array<[Selector<typeof o, any>, any, Array<string | number>]> = [
         [
           selectorChain(o).a.b.c(),
           o.a.b.c,
@@ -175,7 +62,7 @@ describe('#selectors', function () {
         ]
       ] as any
     
-    const selector = selectorChain(o).a.b.c()
+    //const selector = selectorChain(o).a.b.c()
     
     for (const [[value, keyPath], testValue, testKeyPath] of propChainTests) {
       //const [value, keyPath] = chain()
@@ -185,7 +72,7 @@ describe('#selectors', function () {
       expect(keyPath).to.deep.equal(testKeyPath)
     }
     
-    for (const [selector, testValue, testKeyPath] of selectorChainTests) {
+    for (const [selector, testValue] of selectorChainTests) {
       //const [value, keyPath] = chain()
       const value = selector(o)
       //log.info("Key path", keyPath, "value", value)
